76d9c11ee62fe5c6cf7196dbb767e84e80638a99
muthu1443 <arulsakthi@gmail.com>
2022-03-13 16:01:59 +0530

    new class file addded

---
+++ src/org/task1/Task1.java
@@ -0,0 +1,16 @@
+package org.task1;
+
+public class Task1 {
+	
+	public static void main(String[] args) {
+		
+		int a=10,b=20,c;
+		
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("a value is  "+a);
+		System.out.println("b value is  " +b);
+		
+	}
+}
